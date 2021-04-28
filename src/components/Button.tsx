dea1cfe64637142842bfc4bfa9a2b3216fdee298
Johnatan Dos Santos silva <iroman250@gmail.com>
2021-04-28 18:25:19 -0300

    Ajustando component button para todas as pages

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,30 +1,35 @@
 import React from 'react';
 import { TouchableOpacity, Text, StyleSheet, TouchableOpacityProps } from 'react-native';
 import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 
-interface ButtonProps extends TouchableOpacityProps {
-  title: string;
-}
+// interface ButtonProps extends TouchableOpacityProps {
+//   title: string;
+// }
 
-export function Button({ title, ...rest }: ButtonProps) {
+export function Button() {
   return (
-    <></>
+    <TouchableOpacity style={styles.container}>
+      <Text style={styles.text} >
+        Confirmar
+      </Text>
+    </TouchableOpacity>
   )
 }
 
 const styles = StyleSheet.create({
-  button: {
+  container: {
     backgroundColor: colors.green,
     justifyContent: 'center',
     alignItems: 'center',
     borderRadius: 16,
     marginBottom: 48,
     height: 56,
-    width: 56,
   },
-  image: {
-    width: 292,
-    height: 284
+  text: {
+    fontSize: 16,
+    color: colors.white,
+    fontFamily: fonts.heading,
   },
   buttonText: {
     color: colors.white,
