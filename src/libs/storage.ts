c04f6dd299a61fe5ba3364229e8ba2f5496aa671
Johnatan Dos Santos silva <iroman250@gmail.com>
2021-05-05 15:21:15 -0300

    Interface para salvar as plantas no asyncStorage

--- src/libs/storage.ts
+++ src/libs/storage.ts
@@ -13,5 +13,37 @@ export interface PlantProps {
     times: number;
     repeat_every: string;
   };
-  dateTimeNotifcation: Date; 
+  dateTimeNotifcation: Date;
+}
+
+interface StoragePlantProps {
+  [id: string]: {
+    data: PlantProps;
+  };
+}
+
+export async function savePlant(plant: PlantProps): Promise<void> {
+  try {
+    const data = await AsyncStorage.getItem("@plantmanager:plants");
+    // oldPlants vai retornar data se tiver dados na variavel um jsonParse com a tipagem de StoragePlantProps
+    // caso estiver vazio retorna um objeto Vazio no lugar
+    const oldPlants = data ? (JSON.parse(data) as StoragePlantProps) : {};
+
+    // Variavel pega o id da planta atual para setar uma nova planta nesse obejto
+    const newPlant = {
+      [plant.id]: {
+        data: plant,
+      },
+    };
+
+    // seto os dados novos das plantas e tbm os dados antigos e assim quando salvar nao sobrescreveremos os dados antigos
+    await AsyncStorage.setItem(
+      "@plantmanager:plants",
+      JSON.stringify({
+        ...newPlant,
+        ...oldPlants,
+      }));
+  } catch (error) {
+    throw new Error(error);
+  }
 }
