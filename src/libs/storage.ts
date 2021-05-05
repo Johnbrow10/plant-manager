522ffbd969de7d9f76763b3ae864c40e1a3d163e
Johnatan Dos Santos silva <iroman250@gmail.com>
2021-05-05 16:26:19 -0300

    Map e sorted para listar os dados gerais das plantas salvar com formatações nas datas

--- src/libs/storage.ts
+++ src/libs/storage.ts
@@ -49,15 +49,34 @@ export async function savePlant(plant: PlantProps): Promise<void> {
   }
 }
 
-export async function loadPlant(): Promise<StoragePlantProps> {
+export async function loadPlant(): Promise<PlantProps[]> {
   try {
     const data = await AsyncStorage.getItem("@plantmanager:plants");
     // plants vai retornar data se tiver dados na variavel um jsonParse com a tipagem de StoragePlantProps
     // caso estiver vazio retorna um objeto Vazio no lugar
     const plants = data ? (JSON.parse(data) as StoragePlantProps) : {};
 
-    return plants;
-
+    // faz um map apra pegar cada planta salva com todos os dados e depois incluimos um campo de hora atualizada e formatada com a hora e minuto
+    const plantsSorted = Object.keys(plants)
+      .map((plant) => {
+        return {
+          ...plants[plant].data,
+          hour: format(
+            new Date(plants[plant].data.dateTimeNotifcation),
+            "HH:mm"
+          ),
+        };
+      })
+      .sort(
+        (a, b) =>
+          Math.floor(
+            new Date(a.dateTimeNotifcation).getTime() / 1000 -
+              Math.floor(new Date(b.dateTimeNotifcation).getTime() / 1000)
+          )
+        // ja esse sort va trazer dois campos que terao o A e b para capturar a primeira hora e depois a segunda hora e vai fazer menos a maior hora pela segunda
+      );
+    return plantsSorted;
+  
   } catch (error) {
     throw new Error(error);
   }
