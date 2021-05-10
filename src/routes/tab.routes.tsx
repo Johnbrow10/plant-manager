142b49666241ce9ab48ab54a8cbef47a70e55b26
Johnatan Dos Santos silva <iroman250@gmail.com>
2021-05-10 15:34:43 -0300

    Ajustando padding para cada tipo de OS mobile

--- src/routes/tab.routes.tsx
+++ src/routes/tab.routes.tsx
@@ -1,6 +1,7 @@
 import { MaterialIcons } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React from 'react';
+import { Platform } from 'react-native';
 import { MyPlants } from '../pages/MyPlants';
 import { PlantSelect } from '../pages/PlantSelect';
 import colors from '../styles/colors';
@@ -13,9 +14,10 @@ const AuthRoutes = () => {
       tabBarOptions={{
         activeTintColor: colors.green,
         inactiveTintColor: colors.heading,
+
         labelPosition: 'beside-icon',
         style: {
-          paddingVertical: 20,
+          paddingVertical: Platform.OS === 'ios' ? 20 : 0,
           height: 88
         },
       }}
